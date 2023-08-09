76a6effd48141ed964b1cbb30625766cc1043f68
chrisonam <chrisonambs@gmail.com>
2023-08-09 20:15:17 -0300

    JavaProject1 implemented

--- JavaProject1/src/main/java/com/mycompany/javaproject1/JavaProject1.java
+++ JavaProject1/src/main/java/com/mycompany/javaproject1/JavaProject1.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.javaproject1;
+import java.util.LinkedList;
 
 /**
  *
@@ -11,6 +12,39 @@ package com.mycompany.javaproject1;
 public class JavaProject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String mem;
+        
+        MembershipManagement mm = new MembershipManagement();
+        
+        FileHandler fh = new FileHandler();
+        
+        LinkedList<Member> members = fh.readFile();
+        int choice = mm.getChoice();
+        
+        while (choice != -1){
+            switch (choice){
+                case 1 -> {
+                    mem = mm.addMembers(members);
+                    fh.appendFile(mem);
+                }
+                
+                case 2 -> {
+                    mm.removeMember(members);
+                    fh.overwriteFile(members);
+                }
+                
+                case 3 ->{
+                    mm.printMemberInfo(members);
+                }
+                default ->{
+                System.out.print("\n You have selected an invalid option.\n");
+                }  
+            }
+            
+            choice = mm.getChoice();
+            }
+        }
+        
+        
     }
-}
+
